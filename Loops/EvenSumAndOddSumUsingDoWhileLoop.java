f84ee35a99c4f627da8ecd0c27fed9c27dd37734
Sai Sree Rachamalla <83073078+saisree-rachamalla@users.noreply.github.com>
2024-10-01 19:11:17 -0500

    Add files via upload

---
+++ Loops/EvenSumAndOddSumUsingDoWhileLoop.java
@@ -0,0 +1,39 @@
+package com.sai.sree.javabasics;
+import java.util.*;
+public class EvenSumAndOddSumUsingDoWhileLoop 
+{
+	public static void main(String args[]) 
+	{
+
+        Scanner sc = new Scanner(System.in);
+        int number;
+        int choice;
+        int evensum = 0;
+        int oddsum = 0;
+
+        
+        do{
+            System.out.println("Enter the number");
+            number = sc.nextInt();
+
+            if(number % 2 ==0 )
+            {
+                evensum += number;                          // EvenSum and OddSum.
+            }
+            else
+            {
+                oddsum += number;                            
+            }
+
+            System.out.println("Do you want to continue? Press 1 for yesor 0 for no");
+
+            choice = sc.nextInt();
+
+        }while(choice==1);
+
+        System.out.println("Sum of even numbers sum: " + evensum);
+        System.out.println("Sum of odd numbers sum: " + oddsum);
+		
+	}
+
+}
---
+++ Loops/MultiplicationTable.java
@@ -0,0 +1,20 @@
+package com.sai.sree.javabasics;
+import java.util.*;
+public class MultiplicationTable 
+{
+	public static void main(String args[]) 
+	{
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+
+        int n = sc.nextInt();                           //Multiplication Table.
+
+        for(int i = 1; i <= 10; i++){
+            System.out.println(n +  " * "+ i + " = " + n*i);
+        }
+
+		
+	}
+
+}
