7ffcd0bdb4fefdee91d5d850156668fad7a40cb9
Sai Sree Rachamalla <83073078+saisree-rachamalla@users.noreply.github.com>
2024-10-01 22:06:26 -0500

    Update CharaterPattern.java

--- 5. Patterns(Basic)/CharaterPattern.java
+++ 5. Patterns(Basic)/CharaterPattern.java
@@ -3,14 +3,14 @@
 public class CharaterPattern 
 {
 	public static void main(String args[]) 
-	 {
+	 {								//  Charater Pattern 
 	        int n = 6;
-	        char ch = 'A';                               
-	        for(int line =1; line <=n; line++)
-	        {
-	            for(int chars = 1; chars <= line; chars++)
-	            {
-	                System.out.print(ch);
+	        char ch = 'A';						//    A                               
+	        for(int line =1; line <=n; line++)			//    BC
+	        {							//    DEF
+	            for(int chars = 1; chars <= line; chars++)		//    GHIJ
+	            {							//    KLMNO
+	                System.out.print(ch);				//    PQRSTU
 	                ch++;
 	            }
 	            System.out.println();
