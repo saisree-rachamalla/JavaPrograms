a6b6c9f4ac72eca9174cba274361d29539ab6924
Sai Sree Rachamalla <83073078+saisree-rachamalla@users.noreply.github.com>
2024-10-01 19:27:39 -0500

    Add files via upload

---
+++ Arrays/MaximumSubArraySumUsingBruteForce.java
@@ -0,0 +1,39 @@
+package com.sai.sree.arrays;
+
+public class MaximumSubArraySumUsingBruteForce 
+{
+	 public static void maxsubarraysum(int numbers[])
+	 {         
+		 									//Maximum Sub Array Sum - Brute Force
+
+	        int currsum = 0;
+	        int maxsum = Integer.MIN_VALUE;
+
+	        for(int i=0; i<numbers.length;i++)
+	        {
+	                int start = i;
+	            for(int j=i; j<numbers.length;j++)
+	            {
+	                int end = j;
+	                currsum = 0;
+	                for(int k=start; k<=end ; k++)
+	                {
+
+	                    currsum += numbers[k];
+	                }
+	                    System.out.println(currsum);
+	                    if(maxsum < currsum)
+	                    {
+	                        maxsum = currsum;
+	                    }
+	                
+	            }
+	        }    
+	    System.out.println("Maximum sum = " + maxsum);
+	    }
+	    public static void main(String args[]){
+	        int numbers[] = {2,4,6,8,10};
+	        maxsubarraysum(numbers);
+	    }
+
+}
---
+++ Arrays/MaximumSubarraySumUsingKadanesAlgorithm.java
@@ -0,0 +1,28 @@
+package com.sai.sree.arrays;
+
+public class MaximumSubarraySumUsingKadanesAlgorithm 
+{
+	 public static void kadanes(int numbers[])            //Maximum Subarray Sum using kadanes algorithm
+	    {
+	        int maxsum = Integer.MIN_VALUE;
+	        int currsum = 0;
+
+	        for(int i=0;i<numbers.length;i++)
+	        {
+	            currsum = currsum + numbers[i];
+	            if(currsum < 0)
+	            {
+	                currsum = 0;
+	            }
+	            maxsum = Math.max(currsum,maxsum);
+	        }
+	        System.out.println("Maximum Subarray Sum is : " + maxsum);
+	    }
+
+	    public static void main(String args[])
+	    {
+	        int numbers[] = {-2,-3,4,-1,-2,1,5,-3};
+	        kadanes(numbers);
+	    }
+
+}
