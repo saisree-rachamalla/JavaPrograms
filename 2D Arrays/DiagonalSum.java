0455e840979d967bc46d8a45443dd6faaf42ff28
Sai Sree Rachamalla <83073078+saisree-rachamalla@users.noreply.github.com>
2024-10-01 19:33:20 -0500

    Add files via upload

---
+++ 2D Arrays/DiagonalSum.java
@@ -0,0 +1,61 @@
+package com.sai.sree.matrices;
+
+public class DiagonalSum 
+{
+
+    public static int diagonalSum(int matrix[][])         // Printing diagonal sum in 2D array.
+    {
+        int sum = 0;
+
+        for(int i =0; i< matrix.length;i++)                  // o(n^2)
+        {
+            for(int j =0 ;j<matrix[0].length;j++)
+            {
+                if(i == j)
+                {
+                    sum+= matrix[i][j];
+                }
+                else if( i+j == matrix.length-1)
+                {
+                    sum+= matrix[i][j];
+                }
+            }
+        }
+        return sum;
+    }
+
+
+
+            //  Another way with o(n) time complexity
+
+        public static int diagonalsum(int matrix[][])
+        {
+            int sum = 0;
+
+            for(int i=0;i<matrix.length;i++)                            //o(n)
+            {
+                //primary diagonal
+                sum+= matrix[i][i];
+                //secondary diagonal
+                if(i!= matrix.length-1-i)
+                    sum+= matrix[i][matrix.length-1-i]; 
+
+            }
+            return sum;
+        }
+
+    public static void main(String args[])
+    {
+        int matrix[][] = {{1, 2, 3, 4},
+                          {5, 6, 7, 8},
+                          {9, 10, 11, 12},
+                          {13, 14, 15, 16}};
+
+           System.out.println(diagonalSum(matrix));  //-------> o(n^2)
+           System.out.println(diagonalsum(matrix));  //-------> o(n)
+           
+
+
+    }
+
+}
---
+++ 2D Arrays/SearchAnElementInAIn2DArray.java
@@ -0,0 +1,52 @@
+package com.sai.sree.matrices;
+import java.util.*;
+public class SearchAnElementInAIn2DArray 
+{
+	  public static boolean search(int matrix[][], int key)         //Search an element in an 2D Array.
+	  {      
+	        
+	        for(int i=0;i<matrix.length;i++)
+	        {
+	            for(int j=0;j<matrix[0].length;j++)
+	            {
+	                if(matrix[i][j]==key)
+	                {
+	                    System.out.print("found cell at (" + i + "," + j + ")");
+	                    return true;
+	                }
+	            }
+	        }
+	        System.out.println("Key not found");
+	        return false;
+	        
+	    }
+	   
+	    public static void main(String args[])
+	    {
+	        int matrix [][] = new int[3][3];                        //creation of 2D Array.
+	        int n = matrix.length, m = matrix[0].length;
+	        Scanner sc = new Scanner(System.in);
+	        for(int i =0; i<n;i++)
+	        {
+	            for(int j=0; j<m;j++)
+	            {
+	                matrix[i][j] = sc.nextInt();
+
+	            }
+
+	        } 
+
+	        for(int i=0;i<n;i++)
+	        {
+	            for(int j=0; j<m; j++)
+	            {
+	                System.out.print(matrix[i][j]+ " ");
+	            }
+	            System.out.println();
+	        }
+	         search(matrix,5);
+
+	    } 
+
+
+}
