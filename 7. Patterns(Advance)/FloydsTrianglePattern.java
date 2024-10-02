8de05adef92926f860ff9ec84b2b5ea1405f59ed
Sai Sree Rachamalla <83073078+saisree-rachamalla@users.noreply.github.com>
2024-10-01 20:42:58 -0500

    Add files via upload

---
+++ 7. Patterns(Advance)/FloydsTrianglePattern.java
@@ -0,0 +1,23 @@
+package com.saisree.patternspartwo;
+
+public class FloydsTrianglePattern 
+{
+	 public static void floyds_triangle(int n){
+	      int counter = 1;
+	     //outer loop
+	      for(int i = 1; i<=n; i++){
+	          // inner loop - how many times will counter be printed.
+	          for(int j = 1; j <= i; j++){
+	              System.out.print(counter + " ");              
+	              counter++;
+	              
+	          }
+	          System.out.println();                       // Floyds_triangle Pattern
+	      }                                                  //    1
+	  }                                                       //   2 3 
+		public static void main(String[] args) {              //   4 5 6 
+		    floyds_triangle(4);                                //  7 8 9 10 
+		    
+		}
+
+}
