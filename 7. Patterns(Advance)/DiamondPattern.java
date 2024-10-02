8e77d10acbc848afb827ada9b21dca35b73a5899
Sai Sree Rachamalla <83073078+saisree-rachamalla@users.noreply.github.com>
2024-10-01 22:02:14 -0500

    Update DiamondPattern.java

--- 7. Patterns(Advance)/DiamondPattern.java
+++ 7. Patterns(Advance)/DiamondPattern.java
@@ -4,14 +4,14 @@ public class DiamondPattern
 {
 	public static void diamond(int n){
 	      // 1st half
-	      for(int i = 1; i<=n; i++){
-	          
-	          //spaces
-	          for(int j =1 ; j <= (n-i); j++){
-	              System.out.print(" ");
-	          }
-	          
-	          //stars
+	      for(int i = 1; i<=n; i++){			 //      Diamond Pattern
+	          								
+	          //spaces					//             *
+	          for(int j =1 ; j <= (n-i); j++){	         //          * * *
+	              System.out.print(" ");			 //        * * * * * 
+	          }						 //        * * * * *
+	          						  //         * * *
+	          //stars					//             *
 	          for(int j = 1; j <= (2*i)-1; j++){
 	              System.out.print("*");
 	          }
@@ -27,14 +27,14 @@ public class DiamondPattern
 	              System.out.print(" ");
 	          }
 	          
-	          //stars                                       //      Diamond Pattern
+	          //stars                                      
 	          for(int j = 1; j <= (2*i)-1; j++){
-	              System.out.print("*");                    //             *
-	          }                                              //          * * *
-	          System.out.println();                          //        * * * * *
-	      }                                                  //        * * * * *
-	  }                                                       //         * * *
-		public static void main(String[] args) {           //          *
+	              System.out.print("*");                    
+	          }                                           
+	          System.out.println();                         
+	      }                                                 
+	  }                                                     
+		public static void main(String[] args) {           
 			diamond(5);
 		}
 
