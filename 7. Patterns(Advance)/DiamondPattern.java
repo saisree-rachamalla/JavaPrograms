c082017f6c084f801924cfadc58d53138e139653
Sai Sree Rachamalla <83073078+saisree-rachamalla@users.noreply.github.com>
2024-10-01 21:02:22 -0500

    Update DiamondPattern.java

--- 7. Patterns(Advance)/DiamondPattern.java
+++ 7. Patterns(Advance)/DiamondPattern.java
@@ -34,7 +34,7 @@ public class DiamondPattern
 	          System.out.println();                          //        * * * * *
 	      }                                                  //        * * * * *
 	  }                                                       //         * * *
-		public static void main(String[] args) {               //          *
+		public static void main(String[] args) {               //      *
 			diamond(5);
 		}
 
