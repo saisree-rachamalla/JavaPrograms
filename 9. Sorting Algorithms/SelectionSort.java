8e4b1b706910067f6033e2d7e1a724f6f3cb2f5b
Sai Sree Rachamalla <83073078+saisree-rachamalla@users.noreply.github.com>
2024-10-01 21:10:56 -0500

    Update SelectionSort.java

--- 9. Sorting Algorithms/SelectionSort.java
+++ 9. Sorting Algorithms/SelectionSort.java
@@ -3,7 +3,7 @@ package com.saisree.basicsorting;
 public class SelectionSort 
 {
 	public static void selectionSort(int arr[])
-	{                  										//Selection Sort
+	{                  				 //Selection Sort
         for(int i=0;i<arr.length-1;i++){
             int minpos = i;
             for(int j=i+1; j<arr.length;j++){
