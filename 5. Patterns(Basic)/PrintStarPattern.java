985557c2a3951f40e8c533152294598a88f86040
Sai Sree Rachamalla <83073078+saisree-rachamalla@users.noreply.github.com>
2024-10-01 22:15:48 -0500

    Update PrintStarPattern.java

--- 5. Patterns(Basic)/PrintStarPattern.java
+++ 5. Patterns(Basic)/PrintStarPattern.java
@@ -3,12 +3,13 @@
 public class PrintStarPattern 
 {
 
-	 public static void main(String args[]) {
-	        for(int line = 1; line <= 4; line++)
-	        {
-	            for(int star = 1; star <= line; star++)
-	            {
-	                System.out.print("*");              
+	 public static void main(String args[]) 
+	{							//  Star Pattern 
+	        for(int line = 1; line <= 4; line++)		
+	        {						  //     *
+	            for(int star = 1; star <= line; star++)	  //     **
+	            {						 //      ***
+	                System.out.print("*");                   //      ****
 	            }
 	            System.out.println();
 	        }
