bc55984b0ce2e995c27300fc2183360df42b1cbc
Sai Sree Rachamalla <83073078+saisree-rachamalla@users.noreply.github.com>
2024-10-01 20:46:02 -0500

    Add files via upload

---
+++ 13. OOPS/AbstarctClassAndAbstarctFunctions.java
@@ -0,0 +1,46 @@
+package com.saisree.oops;
+
+public class AbstarctClassAndAbstarctFunctions 
+{
+	 public static void main(String args[])
+	    {
+	        Horse h = new Horse();             // Implementing Abstarct class and Abstarct Functions
+	        h.eat();
+	        h.walk();
+
+	        Chicken c = new Chicken();
+	        c.eat();
+	        c.walk();
+
+	    }
+	}
+
+	abstract class Animal1                                    // Abstarct class
+	{
+	    void eat()
+	    {
+	        System.out.println("eats");
+	    }
+
+	    abstract void walk();                                 //Abstarct Function
+	}
+
+	class Horse extends Animal1
+	{
+		
+	    void walk()
+	    {
+	        System.out.println("walks on 4 legs");
+
+	    }
+	}
+
+	class Chicken extends Animal1
+	{
+	    void walk()
+	    {
+	        System.out.println("walks on 2 legs");
+
+	    }
+
+}
---
+++ 13. OOPS/Inheritance.java
@@ -0,0 +1,43 @@
+package com.saisree.oops;
+
+public class Inheritance 
+{
+	 public static void main(String args[])
+	    {
+	        Fish shark = new Fish();                      
+	        shark.eat();
+	    }
+
+	}
+
+	//Base Class or parent class                                  //Inheritance 
+	class Animal
+	{
+	    String color;
+
+	    void eat()
+	    {
+	        System.out.println("eats");
+	    }
+
+	    void breathe()
+	    {
+	        System.out.println("breathes");
+
+	    }
+
+	}
+
+
+	//Derived class or sub class or child class                    //Single level Inheritance
+	class Fish extends Animal
+	{
+	    int fins;
+
+	    void swim()
+	    {
+	        System.out.println("swims in water");
+
+	    }
+
+}
