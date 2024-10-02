2d220ba9a2ac9c6c02a68191cb5e747dfead4643
Sai Sree Rachamalla <83073078+saisree-rachamalla@users.noreply.github.com>
2024-10-01 22:12:55 -0500

    Update InvertedStarPattern.java

--- 5. Patterns(Basic)/InvertedStarPattern.java
+++ 5. Patterns(Basic)/InvertedStarPattern.java
@@ -3,14 +3,15 @@
 public class InvertedStarPattern 
 {
 	 public static void main(String args[]) 
-	 {
-		 int n = 6;
-	        for(int line = 1; line <= n; line++)
-	        {
-	            for(int star = 1; star <= n-line+1; star++)
-	            {
-	                System.out.print("*");                   
-	            }
+	 {							    // Inverted Star Pattern 
+		 int n = 6;			
+	        for(int line = 1; line <= n; line++)		     //     ******
+	        {						    //      *****
+	            for(int star = 1; star <= n-line+1; star++)	    //      ****
+	            {						   //       ***
+	                System.out.print("*");                     //       **
+	            }						   //       *
+
 	            System.out.println();
 	        }
 	 }	
